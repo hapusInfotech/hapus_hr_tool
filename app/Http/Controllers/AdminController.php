54e91990d584df7e7996e7349e2b0ce07c4f6f24
Anandaakrishnan-GA <anandaakrishnan.ga@hapusinfotech.com>
2024-10-15 23:03:54 +0530

    admin dashboard page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+
 class AdminController extends Controller
 {
     /**
@@ -11,9 +13,30 @@ class AdminController extends Controller
      */
     public function super_admin_index()
     {
-        // You can pass any data to the view if needed
-        return view('admin.home');
+        // Fetch data from subscriptions and subscription_details using join
+        $subscriptions = DB::table('subscriptions')
+            ->join('subscription_details', 'subscriptions.id', '=', 'subscription_details.subscription_id')
+            ->select(
+                'subscriptions.id',
+                'subscriptions.type',
+                'subscriptions.uid',
+                'subscriptions.status',
+                'subscriptions.plan',
+                'subscriptions.created_at',
+                'subscriptions.updated_at',
+                'subscription_details.name',
+                'subscription_details.phone',
+                'subscription_details.email',
+                'subscription_details.address',
+                'subscription_details.country',
+                'subscription_details.payment_status'
+            )
+            ->get();
+    
+        // Pass the data to the view
+        return view('admin.home', ['subscriptions' => $subscriptions]);
     }
+    
     public function support_admin_index()
     {
         // You can pass any data to the view if needed
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,15 +1,185 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Hapus Spper Admin</title>
+  <!-- base:css -->
+
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="{{ asset('admin_asset/css/vertical-layout-light/style.css') }}">
+
 </head>
+
 <body>
-    <h1>Admin</h1>
-    @extends('layouts.common.commonUserDashboard')
-    @section('content')
-    @endsection
+
+  <div class="container-scroller">
+    <!-- partial:partials/_navbar.html -->
+    <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+      <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+        <a class="navbar-brand brand-logo" href="{{ route('admin.home') }}"><img
+            src="https://hapusinfotech.com/sites/default/files/hapus_logo_1.png" alt="logo" /></a>
+
+      </div>
+      <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+        <ul class="navbar-nav mr-lg-2">
+          <li class="nav-item  d-none d-lg-flex">
+            <a class="nav-link" href="#">
+              Calendar
+            </a>
+          </li>
+          <li class="nav-item  d-none d-lg-flex">
+            <a class="nav-link active" href="#">
+              Statistic
+            </a>
+          </li>
+          <li class="nav-item  d-none d-lg-flex">
+            <a class="nav-link" href="#">
+              Employee
+            </a>
+          </li>
+        </ul>
+        <ul class="navbar-nav navbar-nav-right">
+          <li class="nav-item d-none d-lg-flex  mr-2">
+            <a class="nav-link" href="#">
+              Help
+            </a>
+          </li>
+
+
+          <li class="nav-item nav-profile dropdown">
+            <a class="nav-link dropdown-toggle  pl-0 pr-0" href="#" data-toggle="dropdown" id="profileDropdown">
+              <i class="typcn typcn-user-outline mr-0"></i>
+              <span class="nav-profile-name">Evan Morales</span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
+              <a class="dropdown-item">
+                <i class="typcn typcn-cog text-primary"></i>
+                Settings
+              </a>
+              <a class="dropdown-item">
+                <i class="typcn typcn-power text-primary"></i>
+                Logout
+              </a>
+            </div>
+          </li>
+        </ul>
+        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button"
+          data-toggle="offcanvas">
+          <span class="typcn typcn-th-menu"></span>
+        </button>
+      </div>
+    </nav>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+
+      <!-- partial:partials/_sidebar.html -->
+      <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+          <li class="nav-item">
+            <div class="d-flex sidebar-profile">
+              <div class="sidebar-profile-image">
+                <img src="{{ asset('assets/img/profile.jpg')}}" alt="image">
+                <span class="sidebar-status-indicator"></span>
+              </div>
+              <div class="sidebar-profile-name">
+                <p class="sidebar-name">
+                  Kenneth Osborne
+                </p>
+                <p class="sidebar-designation">
+                  Welcome
+                </p>
+              </div>
+            </div>
+            <div class="nav-search">
+              <div class="input-group">
+                <input type="text" class="form-control" placeholder="Type to search..." aria-label="search"
+                  aria-describedby="search">
+                <div class="input-group-append">
+                  <span class="input-group-text" id="search">
+                    <i class="typcn typcn-zoom"></i>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <p class="sidebar-menu-title">Dash menu</p>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('admin.home') }}">
+              <i class="typcn typcn-device-desktop menu-icon"></i>
+              <span class="menu-title">Dashboard <span class="badge badge-primary ml-3">New</span></span>
+            </a>
+          </li>
+          <!-- Add the rest of the sidebar navigation items -->
+        </ul>
+      </nav>
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-12 d-flex grid-margin stretch-card">
+                <div class="card">
+                  <div class="card-body">
+                    <div class="d-flex flex-wrap justify-content-between">
+                      <h4 class="card-title mb-3">Payment Transaction Detail</h4>
+                    </div>
+                    <div class="table-responsive">
+                      <table class="table table-bordered table-striped">
+                        <thead class="thead-dark">
+                          <tr>
+                            <th>Name</th>
+                            <th>Phone</th>
+                            <th>Email</th>
+                            <th>Address</th>
+                            <th>Country</th>
+                            <th>Plan</th>
+                            <th>Status</th>
+                            <th>Payment Status</th>
+                            <th>Created At</th>
+                            <th>Updated At</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          @foreach($subscriptions as $subscription)
+                          <tr>
+                            <td>{{ $subscription->name }}</td>
+                            <td>{{ $subscription->phone }}</td>
+                            <td>{{ $subscription->email }}</td>
+                            <td>{{ $subscription->address }}</td>
+                            <td>{{ $subscription->country }}</td>
+                            <td>{{ $subscription->plan }}</td>
+                            <td>{{ $subscription->status }}</td>
+                            <td>{{ $subscription->payment_status }}</td>
+                            <td>{{ $subscription->created_at }}</td>
+                            <td>{{ $subscription->updated_at }}</td>
+                          </tr>
+                          @endforeach
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <!-- main-panel ends -->
+  </div>
+  <!-- page-body-wrapper ends -->
+  </div>
+
+
+  <script src="{{ asset('admin_asset/js/dashboard.js') }}"></script>
+  <!-- End custom js for this page-->
 </body>
+
 </html>
